d868f4860f44cc4b813df7f16b28c7dfe32e37c9
hiddevuijk <hdvuijk@gmail.com>
2015-11-21 17:52:35 +0100

    normal calculates and plots eigenvalues

--- random/headers/write_matrix.h
+++ random/headers/write_matrix.h
@@ -5,10 +5,11 @@
 
 #include <iostream>
 #include <iomanip>
-
+#include <fstream>
 template<typename Vec>
-void write_matrix(const Vec &v, const int size, std::ostream &outstream,int prec = 16, char delimiter='\n')
+void write_matrix(const Vec &v, const int size, string name,int prec = 16, char delimiter='\n')
 {
+	ofstream outstream(name);
 	outstream << std::setprecision(prec);
 	for(int i=0;i<size;i++) outstream << v[i] << delimiter;
 }
--- random/main.cpp
+++ random/main.cpp
@@ -1,30 +1,66 @@
 #include </usr/local/Cellar/eigen/3.2.4/include/eigen3/Eigen/Dense>
 
+#include "nr_headers/nr3.h"
+#include "nr_headers/ran.h"
+
 #include <vector>
 #include <iostream>
 #include <fstream>
 #include <string> 
 #include <sstream>
+#include <math.h>
 
 using namespace::std;
 using namespace::Eigen;
 
-#include "headers/read_FLN.h"
 #include "headers/write_matrix.h"
-
+#include "headers/box_muller.h"
 int main()
 {
 
-	MatrixXcf ew(NN,NN);
+	int N = 100;
+	double sparsity = 1.;
+	double mean = 0.0;
+	double var = 1.0;
+
+	int seed = 123456789;
+	Ran r(seed);
+
+	MatrixXcf w(N,N);
+	for(int i=0;i<N;i++) for(int j=0;j<N;j++) {
+		if(r.doub() < sparsity) {
+			w(i,j) = mean + var*bm_transform(r);
+		} else {	
+			w(i,j) = 0.0;
+		}
+	}
+	for(int i=0;i<N;i++) {
+		double a;
+		if(r.doub()>0.5) {
+			a = 1.;
+		} else {
+			a = -1.;
+		}
+		for(int j=0;j<N;j++) {
+			w(i,j) = a*abs(w(i,j));
+		}
+	}
+			
 
 	// Eigen object for computing eigensystem
 	ComplexEigenSolver<MatrixXcf> eigenw;
-	eigenw.compute(ew);
+	eigenw.compute(w);
 
-	eval_re[i] = eigenw.eigenvalues()[i].real();
-	eval_im[i] = eigenw.eigenvalues()[i].imag();
-	evec[j][N-1-i]= eigenw.eigenvectors()(j,i).real();
+	vector<double> eval_re(N);
+	vector<double> eval_im(N);
 
+	for(int i=0;i<N;i++) {
+		eval_re[i] = eigenw.eigenvalues()[i].real();
+		eval_im[i] = eigenw.eigenvalues()[i].imag();
+	}
+
+	write_matrix(eval_re,N,"eval_re.csv");	
+	write_matrix(eval_im,N,"eval_im.csv");
 
 	return 0;
 }
